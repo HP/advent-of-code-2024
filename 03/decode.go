8d27e6ced00876ddb355d932baf7a6958ec44edf
Hans Petter Wilhelmsen <hanspetter@gmail.com>
2024-12-07 21:31:08 +0100

    Solve part 2 of day 3

--- 03/decode.go
+++ 03/decode.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/HP/advent-of-code-2024/utils"
 )
@@ -21,11 +22,25 @@ func main() {
 		code += line
 	}
 
-	re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+	re := regexp.MustCompile(`(?:do\(\)|don't\(\))?.*?mul\((\d+),(\d+)\)`)
 	matches := re.FindAllStringSubmatch(code, -1)
 
+	doCompute := true
 	result := 0
 	for _, match := range matches {
+
+		if strings.Contains(match[0], "do()") {
+			fmt.Println("do", match[0])
+			doCompute = true
+		}
+		if strings.Contains(match[0], "don't()") {
+			fmt.Println("don't", match[0])
+			doCompute = false
+		}
+		if !doCompute {
+			continue
+		}
+
 		if len(match) == 3 {
 			num1, err1 := strconv.Atoi(match[1])
 			num2, err2 := strconv.Atoi(match[2])
